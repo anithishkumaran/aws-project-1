31444a6a324919dd0836bc65e92ac8dd3ef3914a
anithishkumaran <129111432+anithishkumaran@users.noreply.github.com>
2024-11-20 21:47:17 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_Wnmw4kT5K', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '5b0iiv9ifddu2emrm2dqurlskn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_wgLT80aJ4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7u9fo546jtne0daf01ebkj8nvi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://gzbc76ieo2.execute-api.us-east-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
